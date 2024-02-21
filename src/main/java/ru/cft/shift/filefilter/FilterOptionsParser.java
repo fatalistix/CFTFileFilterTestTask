4a2aff94cd5fd31281c9159b6837fa54bc17849f
fatalistix <slavabal11@outlook.com>
2024-02-21 18:26:14 +0700

    added custom exception, options and arguments parsing to special class (record)

--- src/main/java/ru/cft/shift/filefilter/FilterOptionsParser.java
+++ src/main/java/ru/cft/shift/filefilter/FilterOptionsParser.java
@@ -1,12 +1,14 @@
 package ru.cft.shift.filefilter;
 
 import org.apache.commons.cli.*;
+import ru.cft.shift.filefilter.statistics.FullStatistics;
+import ru.cft.shift.filefilter.statistics.NoStatistics;
+import ru.cft.shift.filefilter.statistics.SimpleStatistics;
+import ru.cft.shift.filefilter.statistics.Statistics;
 
-import java.util.Optional;
-
-public class FilterOptions {
+public class FilterOptionsParser {
     private final Options options = new Options();
-    public FilterOptions() {
+    public FilterOptionsParser() {
         {
             Option outputOption = new Option("o", true, "set result path");
             outputOption.setRequired(false);
@@ -14,7 +16,7 @@ public class FilterOptions {
         }
 
         {
-            Option resultFilesPrefixesOption = new Option("p", false, "set result files prefixes");
+            Option resultFilesPrefixesOption = new Option("p", true, "set result files prefixes");
             resultFilesPrefixesOption.setRequired(false);
             options.addOption(resultFilesPrefixesOption);
         }
@@ -38,8 +40,47 @@ public class FilterOptions {
         }
     }
 
-    public CommandLine parseOptions(String[] args) throws ParseException {
+    public FilterOptions parseOptions(String[] args) throws ParseOptionsException {
         CommandLineParser parser = new DefaultParser();
-        return parser.parse(options, args, false);
+        CommandLine commandLine;
+        try {
+            commandLine = parser.parse(options, args, false);
+        } catch (ParseException e) {
+            throw new ParseOptionsException(e);
+        }
+
+        String outputDirectoryPath;
+        if (commandLine.hasOption('o')) {
+            outputDirectoryPath = commandLine.getOptionValue('o');
+        } else {
+            outputDirectoryPath = ".";
+        }
+
+        String filePrefix;
+        if (commandLine.hasOption('p')) {
+            filePrefix = commandLine.getOptionValue('p');
+        } else {
+            filePrefix = "";
+        }
+
+        boolean append;
+        append = commandLine.hasOption('a');
+
+        boolean simpleStats = commandLine.hasOption('s');
+        boolean fullStats = commandLine.hasOption('f');
+        if (simpleStats && fullStats) {
+            throw new ParseOptionsException("illegal options: cannot use -s and -f options at the same time");
+        }
+
+        Statistics statistics;
+        if (simpleStats) {
+            statistics = new SimpleStatistics();
+        } else if (fullStats) {
+            statistics = new FullStatistics();
+        } else {
+            statistics = new NoStatistics();
+        }
+
+        return new FilterOptions(outputDirectoryPath, filePrefix, append, statistics, commandLine.getArgList());
     }
 }
