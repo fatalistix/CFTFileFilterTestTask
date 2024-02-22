8a1cab1e97efefc8c87bd46444e97affbf5fbd02
fatalistix <slavabal11@outlook.com>
2024-02-22 19:20:16 +0700

    fixed ArithmeticException by setting scale

--- src/main/java/ru/cft/shift/filefilter/statistics/FullStatistics.java
+++ src/main/java/ru/cft/shift/filefilter/statistics/FullStatistics.java
@@ -1,54 +1,65 @@
 package ru.cft.shift.filefilter.statistics;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.math.RoundingMode;
 
 public class FullStatistics implements Statistics {
 
-    private long wroteIntegers = 0;
-    private BigDecimal minIntegerValue = new BigDecimal(0);
-    private BigDecimal maxIntegerValue = new BigDecimal(0);
-    private BigDecimal sumOfIntegerValues = new BigDecimal(0);
-
-    private long wroteFloats = 0;
-    private BigDecimal minFloatValue = new BigDecimal(0);
-    private BigDecimal maxFloatValue = new BigDecimal(0);
-    private BigDecimal sumOfFloatValues = new BigDecimal(0);
-
+    private final NumberStatistics integerStatistics = new NumberStatistics();
+    private final NumberStatistics floatStatistics = new NumberStatistics();
     private long wroteStrings = 0;
     private long shortestStringLength = Long.MAX_VALUE;
     private long longestStringLength = -1;
 
+    private static class NumberStatistics {
+        private long totallyWrote = 0;
+        private BigDecimal minValue = new BigDecimal(0);
+        private BigDecimal maxValue = new BigDecimal(0);
+        private BigDecimal sumOfValues = new BigDecimal(0);
+
+        public void register(String number) {
+            BigDecimal newValue = new BigDecimal(number);
+            if (totallyWrote == 0) {
+                minValue = newValue;
+                maxValue = newValue;
+                sumOfValues = newValue;
+            } else {
+                minValue = minValue.min(newValue);
+                maxValue = maxValue.max(newValue);
+                sumOfValues = sumOfValues.add(newValue);
+            }
+            ++totallyWrote;
+        }
+
+        public String getStatisticsAsString() {
+            return String.format("""
+                        Totally wrote:          %d
+                        Maximum value:          %s
+                        Minimum value:          %s
+                        Sum of all values:      %s
+                        average value:          %s
+                        """,
+                    totallyWrote,
+                    totallyWrote == 0 ? "not available" : maxValue,
+                    totallyWrote == 0 ? "not available" : minValue,
+                    totallyWrote == 0 ? "not available" : sumOfValues,
+                    totallyWrote == 0 ? "not available" : sumOfValues.divide(
+                            new BigDecimal(totallyWrote), 20, RoundingMode.HALF_UP
+                    ));
+        }
+    }
+
     //TODO: Possibly decompose to one class "NumberStatistics"
     @Override
     public void registerInteger(String integerString) {
-        BigDecimal newValue = new BigDecimal(integerString);
-        if (wroteIntegers == 0) {
-            minIntegerValue = newValue;
-            maxIntegerValue = newValue;
-            sumOfIntegerValues = newValue;
-        } else {
-            minIntegerValue = minIntegerValue.min(newValue);
-            maxIntegerValue = maxIntegerValue.max(newValue);
-            sumOfIntegerValues = sumOfIntegerValues.add(newValue);
-        }
-        ++wroteIntegers;
+        integerStatistics.register(integerString);
     }
 
     //TODO: Possibly decompose to one class "NumberStatistics"
     @Override
     public void registerFloat(String floatString) {
-        BigDecimal newValue = new BigDecimal(floatString);
-        if (wroteFloats == 0) {
-            minFloatValue = newValue;
-            maxFloatValue = newValue;
-            sumOfFloatValues = newValue;
-        } else {
-            minFloatValue = minFloatValue.min(newValue);
-            maxFloatValue = maxFloatValue.max(newValue);
-            sumOfFloatValues = sumOfFloatValues.add(newValue);
-        }
-        ++wroteFloats;
+        floatStatistics.register(floatString);
     }
 
     @Override
@@ -60,38 +71,7 @@ public class FullStatistics implements Statistics {
 
     @Override
     public String getStatisticsAsString() {
-        String integersStats = String.format("""
-                        Integers:
-                        Totally wrote:          %d
-                        Maximum value:          %s
-                        Minimum value:          %s
-                        Sum of all integers:    %s
-                        average value:          %s
-                        
-                        """,
-                wroteIntegers,
-                wroteIntegers == 0 ? "not available" : maxIntegerValue,
-                wroteIntegers == 0 ? "not available" : minIntegerValue,
-                wroteIntegers == 0 ? "not available" : sumOfIntegerValues,
-                wroteIntegers == 0 ? "not available" : sumOfIntegerValues.divide(
-                        new BigDecimal(wroteIntegers), RoundingMode.UNNECESSARY
-                ));
-        String floatStats = String.format("""
-                        Floats:
-                        Totally wrote:          %d
-                        Maximum value:          %s
-                        Minimum value:          %s
-                        Sum of all floats:      %s
-                        average value:          %s
-                        
-                        """,
-                wroteFloats,
-                wroteFloats == 0 ? "not available" : maxFloatValue,
-                wroteFloats == 0 ? "not available" : minFloatValue,
-                wroteFloats == 0 ? "not available" : sumOfFloatValues,
-                wroteFloats == 0 ? "not available" : sumOfFloatValues.divide(
-                        new BigDecimal(wroteFloats), RoundingMode.UNNECESSARY
-                ));
+
         String stringStats = String.format("""
                         Strings:
                         Totally wrote:          %d
@@ -101,6 +81,11 @@ public class FullStatistics implements Statistics {
                 wroteStrings,
                 wroteStrings == 0 ? "not available" : longestStringLength,
                 wroteStrings == 0 ? "not available" : shortestStringLength);
-        return integersStats + floatStats + stringStats;
+        return "Integers:\n" +
+                integerStatistics.getStatisticsAsString() +
+                "\nFloats:\n" +
+                floatStatistics.getStatisticsAsString() +
+                "\n" +
+                stringStats;
     }
 }
