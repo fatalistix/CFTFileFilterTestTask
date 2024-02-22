00205ce942269e2f1deca94ed56b4be1537e2ae7
fatalistix <slavabal11@outlook.com>
2024-02-22 14:00:54 +0700

    updated FLOAT_PATTERN regex to match all java's floats

--- src/main/java/ru/cft/shift/filefilter/sorter/StringSorter.java
+++ src/main/java/ru/cft/shift/filefilter/sorter/StringSorter.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class StringSorter {
     private static final String INTEGER_PATTERN = "[+-]?\\d+";
-    private static final String FLOAT_PATTERN = "[+-]?\\d*(\\.\\d*)?([Ee][+-]?\\d+)?";
+    private static final String FLOAT_PATTERN = "[+-]?\\d+(\\.\\d*)?([Ee][+-]?\\d+)?|[+-]?\\.\\d+([Ee][+-]?\\d+)?";
     private final Pattern integerPattern = Pattern.compile(INTEGER_PATTERN);
     private final Pattern floatPattern = Pattern.compile(FLOAT_PATTERN);
 
